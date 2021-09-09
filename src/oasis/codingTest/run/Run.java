1d122e48843bd74a959b93e0b59d78ef7b813a41
garam noh <garam.nohh@gmail.com>
2021-09-09 22:53:15 +0900

    프로그램 실행

---
+++ src/oasis/codingTest/run/Run.java
@@ -0,0 +1,10 @@
+package oasis.codingTest.run;
+
+import oasis.codingTest.controller.LottoController;
+
+public class Run {
+
+	public static void main(String[] args) {
+		new LottoController().main();
+	}
+}
